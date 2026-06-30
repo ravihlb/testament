6a62d7660348e53f838d4f7cb36b53b0e4f46dd5
ravihlb <ravihlb@protonmail.com>
2026-06-30 10:58:00 -0300

    feat(node): add biome, organize order.spec imports

--- node/e2e/tests/order.spec.ts
+++ node/e2e/tests/order.spec.ts
@@ -1,56 +1,60 @@
-import test, { expect, Locator } from "@playwright/test";
-import { DEFAULT_PASSWORD, AcceptedUsernames } from "../data/credentials";
-import LoginForm from "../components/LoginForm";
-import { InventoryItems } from "../data/products";
-import InventoryList from "../components/InventoryList";
-import PrimaryHeader from "../components/PrimaryHeader";
-import CheckoutPage, { CheckoutFormFields } from "../components/CheckoutPage";
-import CheckoutCompletePage from "../components/CheckoutCompletePage";
+import test, { expect, type Locator } from "@playwright/test";
 import CartPage from "../components/CartPage";
+import CheckoutCompletePage from "../components/CheckoutCompletePage";
+import CheckoutPage, {
+	type CheckoutFormFields,
+} from "../components/CheckoutPage";
+import InventoryList from "../components/InventoryList";
+import LoginForm from "../components/LoginForm";
+import PrimaryHeader from "../components/PrimaryHeader";
+import { AcceptedUsernames, DEFAULT_PASSWORD } from "../data/credentials";
+import { InventoryItems } from "../data/products";
 
 test.describe("Flow - Order", async () => {
-  test.beforeEach("Setup", async ({ page }) => {
-    const loginForm = new LoginForm(page);
+	test.beforeEach("Setup", async ({ page }) => {
+		const loginForm = new LoginForm(page);
 
-    await page.goto("/");
-    await loginForm.login(AcceptedUsernames.StandardUser, DEFAULT_PASSWORD);
+		await page.goto("/");
+		await loginForm.login(AcceptedUsernames.StandardUser, DEFAULT_PASSWORD);
 
-    await page.waitForURL("**/inventory.html");
-  });
+		await page.waitForURL("**/inventory.html");
+	});
 
-  test("Order", async ({ page }) => {
-    const inventoryList = new InventoryList(page);
-    const primaryHeader = new PrimaryHeader(page);
-    const cartPage = new CartPage(page);
-    const checkoutPage = new CheckoutPage(page);
-    const checkoutCompletePage = new CheckoutCompletePage(page);
+	test("Order", async ({ page }) => {
+		const inventoryList = new InventoryList(page);
+		const primaryHeader = new PrimaryHeader(page);
+		const cartPage = new CartPage(page);
+		const checkoutPage = new CheckoutPage(page);
+		const checkoutCompletePage = new CheckoutCompletePage(page);
 
-    const itemsToAdd = [
-      InventoryItems.SauceLabsBackpack,
-      InventoryItems.SauceLabsFleeceJacket,
-      InventoryItems.TestallTheThingsTShirtRed
-    ];
+		const itemsToAdd = [
+			InventoryItems.SauceLabsBackpack,
+			InventoryItems.SauceLabsFleeceJacket,
+			InventoryItems.TestallTheThingsTShirtRed,
+		];
 
-    for (const item of itemsToAdd) {
-      await inventoryList.addItemToCart(item);
-    }
+		for (const item of itemsToAdd) {
+			await inventoryList.addItemToCart(item);
+		}
 
-    await primaryHeader.goToCart();
-    await cartPage.clickCheckoutBtn();
+		await primaryHeader.goToCart();
+		await cartPage.clickCheckoutBtn();
 
-    const checkoutFormInfo: CheckoutFormFields = {
-      firstName: 'Ravi',
-      lastName: 'Test',
-      postalCode: '12345',
-    };
+		const checkoutFormInfo: CheckoutFormFields = {
+			firstName: "Ravi",
+			lastName: "Test",
+			postalCode: "12345",
+		};
 
-    await checkoutPage.fillCheckoutForm(checkoutFormInfo);
-    await checkoutPage.clickContinueBtn();
-    await checkoutPage.clickFinishCheckoutBtn();
-    expect(checkoutCompletePage.elements.completeHeader as Locator).toBeVisible();
+		await checkoutPage.fillCheckoutForm(checkoutFormInfo);
+		await checkoutPage.clickContinueBtn();
+		await checkoutPage.clickFinishCheckoutBtn();
+		expect(
+			checkoutCompletePage.elements.completeHeader as Locator,
+		).toBeVisible();
 
-    await checkoutCompletePage.clickBackHomeBtn();
-    await page.waitForURL("**/inventory.html");
-    expect(page.url()).toContain("inventory.html");
-  });
+		await checkoutCompletePage.clickBackHomeBtn();
+		await page.waitForURL("**/inventory.html");
+		expect(page.url()).toContain("inventory.html");
+	});
 });
